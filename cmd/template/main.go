cf6a04aa353e4d8d63e1b601fb10a736ada602f1
silenzio <zhanganjing@pyou.com>
2024-04-09 18:15:41 +0800

    dump

--- cmd/template/main.go
+++ cmd/template/main.go
@@ -13,6 +13,8 @@ import (
 )
 
 func main() {
+	defer saveDump()
+
 	time.Local = time.FixedZone("CST", 8*3600) // 东八
 
 	status := status.GetStatus()
@@ -35,4 +37,12 @@ func main() {
 	sign := <-signalChan
 	logrus.Infof("get os signal=%v", sign)
 	logrus.Infof("get os signal=%v", sign.String())
+}
+
+func saveDump() {
+	if err := recover(); err != nil {
+		logrus.Errorf("panic=%v", err)
+		time.Sleep(1 * time.Second)
+		os.Exit(1)
+	}
 }
